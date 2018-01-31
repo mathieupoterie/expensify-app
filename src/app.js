3bdd3d92f55454524c7479b1797f3693d0fe0437
mathieupoterie <mathieupoterie@gmail.com>
2018-01-31 23:55:20 +0100

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ store.dispatch(addExpense({ description: 'REnt', amount: 487349 }));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+
 
 const jsx = (
     <Provider store={store}>
