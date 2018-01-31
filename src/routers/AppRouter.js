9e2a56dccca4f4c2126633c4b1962cb2912de9ef
mathieupoterie <mathieupoterie@gmail.com>
2018-02-01 00:21:01 +0100

    with syntaxic issue on module importing

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import NotFoundPage from '../components/NotFoundPage';
 import AddExpensePage from '../components/AddExpensePage';
-import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import ExpenseDashBoardPage from '../components/ExpenseDashBoardPage';
 import HelpExpensePage from '../components/HelpExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import Header from '../components/Header';
@@ -12,7 +12,7 @@ const AppRouter = () => (
         <div>
             <Header />
             <Switch>
-                <Route path="/" exact={true} component={ExpenseDashboardPage} />
+                <Route path="/" exact={true} component={ExpenseDashBoardPage} />
                 <Route path="/create" component={AddExpensePage} />
                 <Route path="/edit/:id" component={EditExpensePage} />
                 <Route path="/help" component={HelpExpensePage} />
